7e97ae9af09b3689c02c187a2659f7bfbc336e3f
charway <GUchengwei1212>
2017-02-16 09:28:20 +0800

    changing Font to svg type

--- example/index.js
+++ example/index.js
@@ -7,7 +7,7 @@ const LENGTH_MAIN = 350,
     GREEN = 'green',
     RED = '#C5000B',
     ORANGE = 'orange',
-    GRAY = 'gray',
+    GRAY = 'SkyBlue',
     BLACK = '#2B1B17';
 const nodes = [];
 
@@ -16,14 +16,13 @@ const edges = [];
 nodes.push({ id: 1, label: '192.168.0.1', group: 'switch', value: 10 });
 nodes.push({ id: 2, label: '192.168.0.2', group: 'switch', value: 8 });
 nodes.push({ id: 3, label: '192.168.0.3', group: 'switch', value: 6 });
-edges.push({ from: 1, to: 2, length: LENGTH_MAIN, width: WIDTH_SCALE * 6, label: '0.71 mbps' });
-edges.push({ from: 1, to: 3, length: LENGTH_MAIN, width: WIDTH_SCALE * 4, label: '0.55 mbps' });
+edges.push({ from: 1, to: 2, length: LENGTH_MAIN, width: WIDTH_SCALE, label: '0.71 mbps' });
+edges.push({ from: 1, to: 3, length: LENGTH_MAIN, width: WIDTH_SCALE, label: '0.55 mbps' });
 
 // group around 2
 for (let i = 100; i <= 105; i++) {
     let value = 1;
-    let width = WIDTH_SCALE * 2;
-    let color = GRAY;
+    let width = WIDTH_SCALE;
     let label = null;
 
     if (i === 103) {
@@ -31,32 +30,31 @@ for (let i = 100; i <= 105; i++) {
         width = 3;
     }
     if (i === 102) {
-        color = RED;
         label = 'error';
     }
 
     nodes.push({ id: i, label: '192.168.0.' + i, group: 'host', value: value, cid: 1 });
-    edges.push({ from: 2, to: i, length: LENGTH_SUB, color: color, fontColor: color, width: width, label: label });
+    edges.push({ from: 2, to: i, length: LENGTH_SUB, width: width, label: label });
 }
 
 // group around 3
 nodes.push({ id: 202, label: '192.168.0.202', group: 'host', value: 4 });
-edges.push({ from: 3, to: 202, length: LENGTH_SUB, color: GRAY, width: WIDTH_SCALE * 2 });
+edges.push({ from: 3, to: 202, length: LENGTH_SUB, width: WIDTH_SCALE });
 for (let i = 230; i <= 231; i++) {
     nodes.push({ id: i, label: '192.168.0.' + i, group: 'phone', value: 2 });
-    edges.push({ from: 3, to: i, length: LENGTH_SUB, color: GRAY, fontColor: GRAY, width: WIDTH_SCALE });
+    edges.push({ from: 3, to: i, length: LENGTH_SUB, fontColor: GRAY, width: WIDTH_SCALE });
 }
 
 // group around 1
 nodes.push({ id: 10, label: '192.168.0.10', group: 'server', value: 10 });
-edges.push({ from: 1, to: 10, length: LENGTH_SERVER, color: GRAY, width: WIDTH_SCALE * 6, label: '0.92 mbps' });
+edges.push({ from: 1, to: 10, length: LENGTH_SERVER, width: WIDTH_SCALE, label: '0.92 mbps' });
 nodes.push({ id: 11, label: '192.168.0.11', group: 'server', value: 7 });
-edges.push({ from: 1, to: 11, length: LENGTH_SERVER, color: GRAY, width: WIDTH_SCALE * 3, label: '0.68 mbps' });
+edges.push({ from: 1, to: 11, length: LENGTH_SERVER, width: WIDTH_SCALE, label: '0.68 mbps' });
 nodes.push({ id: 12, label: '192.168.0.12', group: 'server', value: 3 });
-edges.push({ from: 1, to: 12, length: LENGTH_SERVER, color: GRAY, width: WIDTH_SCALE, label: '0.3 mbps' });
+edges.push({ from: 1, to: 12, length: LENGTH_SERVER, width: WIDTH_SCALE, label: '0.3 mbps' });
 
 nodes.push({ id: 204, label: 'Internet', group: 'cloud', value: 10 });
-edges.push({ from: 1, to: 204, length: 200, width: WIDTH_SCALE * 3, label: '0.63 mbps' });
+edges.push({ from: 1, to: 204, length: 200, width: WIDTH_SCALE, label: '0.63 mbps' });
 
 const network = new CiscoVis('visualization',
     [{
@@ -77,7 +75,6 @@ const clusterOptionsByData = {
     },
     clusterNodeProperties: {
         id: 'cidCluster',
-        borderWidth: 3,
         label: 'hosts',
         shape: 'icon',
         icon: {
