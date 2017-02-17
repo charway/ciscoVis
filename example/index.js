b6c3979fa813f7d8b5dc4583f8beb5ce8c74a6c3
charway <GUchengwei1212>
2017-02-17 22:12:58 +0800

    change physics setting

--- example/index.js
+++ example/index.js
@@ -14,32 +14,32 @@ const nodes = [
         id: 1,
         label: '192.168.0.1',
         group: 'switch',
-        value: 10
+        //value: 10
     }, {
         id: 2,
         label: '192.168.0.2',
         group: 'router',
-        value: 9
+        //value: 9
     }, {
         id: 3,
         label: '192.168.0.3',
         group: 'accesspoint',
-        value: 9
+        //value: 9
     }, {
         id: 4,
         label: '192.168.0.4',
         group: 'ipphone',
-        value: 6
+        //value: 6
     }, {
         id: 5,
         label: '192.168.0.5',
         group: 'camera',
-        value: 6
+        //value: 6
     }, {
         id: 6,
         label: '192.168.0.6',
         group: 'wirelesshost',
-        value: 8
+        //value: 8
     }, {
         id: 7,
         label: '192.168.0.7',
@@ -52,7 +52,7 @@ const nodes = [
         id: 202,
         label: '192.168.0.202',
         group: 'firewall',
-        value: 4
+        //value: 6
     }, {
         id: 203,
         label: '192.168.0.203',
@@ -61,7 +61,7 @@ const nodes = [
         id: 230,
         label: '192.168.0.230',
         group: 'wlc',
-        value: 2
+        //value: 6
     }, {
         id: 231,
         label: '192.168.0.231',
@@ -75,17 +75,17 @@ const nodes = [
         id: 11,
         label: '192.168.0.11',
         group: 'phone',
-        value: 7
+        //value: 7
     }, {
         id: 12,
         label: '192.168.0.12',
         group: 'nexus5000',
-        value: 10
+        //value: 10
     }, {
         id: 204,
         label: 'Internet',
         group: 'cloud',
-        value: 10
+        //value: 10
     }
 ];
 
@@ -93,85 +93,85 @@ const edges = [
     {
         from: 1,
         to: 2,
-        length: LENGTH_MAIN,
+        //length: LENGTH_MAIN,
         width: WIDTH_SCALE,
         label: '0.71 mbps',
     }, {
         from: 1,
         to: 3,
-        length: LENGTH_MAIN,
+        //length: LENGTH_MAIN,
         width: WIDTH_SCALE,
         label: '0.55 mbps'
     }, {
         from: 3,
         to: 202,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         width: WIDTH_SCALE
     }, {
         from: 4,
         to: 12,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         width: WIDTH_SCALE
     }, {
         from: 5,
         to: 12,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         width: WIDTH_SCALE
     }, {
         from: 6,
         to: 2,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         width: WIDTH_SCALE
     }, {
         from: 8,
         to: 12,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         width: WIDTH_SCALE
     }, {
         from: 7,
         to: 10,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         width: WIDTH_SCALE
     }, {
         from: 3,
         to: 230,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         fontColor: GRAY,
         width: WIDTH_SCALE
     }, {
         from: 3,
         to: 231,
-        length: LENGTH_SUB,
+        //length: LENGTH_SUB,
         fontColor: GRAY,
         width: WIDTH_SCALE
     }, {
         from: 1,
         to: 10,
-        length: LENGTH_SERVER,
+        //length: LENGTH_SERVER,
         width: WIDTH_SCALE,
         label: '0.92 mbps'
     }, {
         from: 10,
         to: 11,
-        length: LENGTH_SERVER,
+        //length: LENGTH_SERVER,
         width: WIDTH_SCALE,
         label: '0.68 mbps'
     }, {
         from: 1,
         to: 12,
-        length: LENGTH_SERVER,
+        //length: LENGTH_SERVER,
         width: WIDTH_SCALE,
         label: '0.3 mbps'
     }, {
         from: 1,
         to: 204,
-        length: 200,
+        //length: 200,
         width: WIDTH_SCALE,
         label: '0.63 mbps'
     }, {
         from: 1,
         to: 203,
-        length: 200,
+        //length: 200,
         width: WIDTH_SCALE,
     }
 ];
@@ -179,7 +179,7 @@ const edges = [
 
 // group around 2
 for (let i = 100; i <= 105; i++) {
-    let value = 5;
+    let value = 8;
     let width = WIDTH_SCALE;
     let label = null;
 
@@ -187,8 +187,8 @@ for (let i = 100; i <= 105; i++) {
         label = 'error';
     }
 
-    nodes.push({ id: i, label: '192.168.0.' + i, group: 'host', value: value, cid: 1 });
-    edges.push({ from: 2, to: i, length: LENGTH_SUB, width: width, label: label });
+    nodes.push({ id: i, label: '192.168.0.' + i, group: 'host', cid: 1 });
+    edges.push({ from: 2, to: i,width: width, label: label });
 }
 
 
